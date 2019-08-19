ef72b1893118e35817df2f6932719ba702ba7b5a
Yuri Leoni de Castro <yuri.castro@sigma.com.br>
2019-08-19 10:21:52 -0400

    criado componente de artigos

--- src/components/Article/index.js
+++ src/components/Article/index.js
@@ -1,9 +1,67 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import './styles.scss';
+
 
 export default function Article({article}) {
+    const [artigo, setArtigo] = useState([]);
+
+    useEffect(() => {
+
+        setArtigo({
+                      id: article.a,
+                      avatar: `https://api.adorable.io/avatars/52/avatars${article.a}.png`,
+                      author: '@leonicastro',
+                      date: 'Agosto 23, 2019',
+                      title: 'titulo de teste',
+                      description: 'descricao de teste de teste',
+                      tagList: ['butt', 'dragons', 'test', 'tags']
+                  }
+        );
+
+
+    }, []);
+
+    function haddleReadMore() {
+
+    }
+
     return (
+        {
+            artigo
+        } &&
         <article>
-            <h3>Artigo {article.a}</h3>
+            <header className="article-header">
+                <picture>
+                    <img className="avatar" src={artigo.avatar}/>
+                    <div className="content">
+                        <div className="author">{artigo.author}</div>
+                        <span className="date">{artigo.date}</span>
+                    </div>
+                </picture>
+                <button className="favorite">
+                    <span className="icon">&#9825;</span> 3
+                </button>
+            </header>
+
+            <div className="article-content">
+                <div>
+                    <h1>{artigo.title}</h1>
+                    <p>{artigo.description}</p>
+                    <span onClick={haddleReadMore}>Read more...</span>
+                </div>
+                <ul className="tag-list">
+                    {artigo.tagList &&
+                    artigo.tagList.map(tag => {
+                        return (
+                            <li className="tag-default tag-pill tag-outline" key={tag}>
+                                {tag}
+                            </li>
+                        );
+                    })
+                    }
+                </ul>
+            </div>
+            <hr/>
         </article>
     );
 }
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,71 +1,79 @@
-import React, { useEffect, useState } from "react";
-import "./styles.scss";
-import Article from "../../components/Article";
-import PopularTags from "../../components/PopularTags";
+import React, { useEffect, useState } from 'react';
+import './styles.scss';
+import Article from '../../components/Article';
+import PopularTags from '../../components/PopularTags';
 
 export default function Home() {
-  const [articles, setArticles] = useState([]);
-  const [feedType, setFeedType] = useState("global");
+    const [articles, setArticles] = useState([]);
+    const [feedType, setFeedType] = useState('global');
 
-  useEffect(() => {
-    function selectFeed() {
-      if (feedType === "global") {
-        setArticles([
-          { a: 2 },
-          { a: 3 },
-          { a: 4 },
-          { a: 5 },
-          { a: 6 },
-          { a: 7 },
-          { a: 8 },
-          { a: 9 },
-          { a: 10 }
-        ]);
-      } else if (feedType === "your") {
-        setArticles([
-          { a: 12 },
-          { a: 13 },
-          { a: 14 },
-          { a: 15 },
-          { a: 16 },
-          { a: 17 },
-          { a: 18 },
-          { a: 19 },
-          { a: 20 }
-        ]);
-      }
-    }
-    selectFeed();
-  }, [feedType]);
+    useEffect(() => {
+        function selectFeed() {
+            if (feedType === 'global') {
+                setArticles([
+                                {a: 2},
+                                {a: 3},
+                                {a: 4},
+                                {a: 5},
+                                {a: 6},
+                                {a: 7},
+                                {a: 8},
+                                {a: 9},
+                                {a: 10}
+                            ]);
+            } else if (feedType === 'your') {
+                setArticles([
+                                {a: 12},
+                                {a: 13},
+                                {a: 14},
+                                {a: 15},
+                                {a: 16},
+                                {a: 17},
+                                {a: 18},
+                                {a: 19},
+                                {a: 20}
+                            ]);
+            }
+        }
+        setArticles([]);
+        setTimeout(() => {
+            selectFeed();
+        }, 2000);
 
-  return (
-    <section className="container">
-      <header className="banner">
-        <h1>conduit</h1>
-        <p>A place to share your knowledge.</p>
-      </header>
-      <section className="main">
-        <section>
-          <nav className="feed-menu">
-            <ul>
-              <li className={feedType === "your" ? "active" : ""}>
-                <button onClick={() => setFeedType("your")}>Your Feed</button>
-              </li>
-              <li className={feedType === "global" ? "active" : ""}>
-                <button onClick={() => setFeedType("global")}>
-                  Global Feed
-                </button>
-              </li>
-            </ul>
-          </nav>
-          {articles.map(article => (
-            <Article key={article.a} article={article} />
-          ))}
+    }, [feedType]);
+
+    return (
+        <section className="container">
+            <header className="banner">
+                <h1>conduit</h1>
+                <p>A place to share your knowledge.</p>
+            </header>
+            <section className="main">
+                <section>
+                    <nav className="feed-menu">
+                        <ul>
+                            <li className={feedType === 'your' ? 'active' : ''}>
+                                <button onClick={() => setFeedType('your')}>Your Feed</button>
+                            </li>
+                            <li className={feedType === 'global' ? 'active' : ''}>
+                                <button onClick={() => setFeedType('global')}>
+                                    Global Feed
+                                </button>
+                            </li>
+                        </ul>
+                    </nav>
+                    {!articles || articles.length === 0 ?
+                        <div>Loading...</div>
+                        :
+                        articles.map(article => (
+                            <Article key={article.a} article={article}/>
+                        ))
+                    }
+                </section>
+                <aside>
+                    <PopularTags/>
+                </aside>
+            </section>
         </section>
-        <aside>
-          <PopularTags />
-        </aside>
-      </section>
-    </section>
-  );
+    );
 }
